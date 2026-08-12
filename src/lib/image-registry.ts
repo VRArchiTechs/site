674f67b4de2deff8c9e0fe833299d84f4b167943
VRArchiTechs <ahirvishnu111@gmail.com>
2026-08-12 21:40:47 +0530

    Add centralized image registry

---
+++ src/lib/image-registry.ts
@@ -0,0 +1,20 @@
+export const images = {
+  work: {
+    tripodResidence: {
+      hero: "/images/work/tri-pod-residence/hero",
+      gallery01: "/images/work/tri-pod-residence/gallery-01",
+    },
+  },
+  visualisation: {
+    livingRoom: {
+      hero: "/images/visualisation/living-room/hero",
+      gallery01: "/images/visualisation/living-room/gallery-01",
+    },
+    diningRoom: {
+      gallery01: "/images/visualisation/dining-room/gallery-01",
+    },
+    bathroom: {
+      gallery01: "/images/visualisation/bathroom/gallery-01",
+    },
+  },
+} as const;
